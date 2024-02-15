4507684f8ad0c054fe0974598843ff266d711ef3
Ben Schumacher <154968490+Aerobiben@users.noreply.github.com>
2024-02-15 12:08:36 +0100

    Update TodoList.js

--- pages/TodoList.js
+++ pages/TodoList.js
@@ -1,26 +1,4 @@
-import { useState } from 'react';
 
-export default function TodoList() {
-  const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState('');
-
-  const addTask = () => {
-    if (newTask.trim() === '') {
-      alert('Please enter a valid task.');
-      return;
-    }
-
-    setTasks([...tasks, newTask]);
-    setNewTask('');
-  };
-
-  const removeTask = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
-  };
-
-  return (
     <div>
       <h1>To-Do List</h1>
       <input
